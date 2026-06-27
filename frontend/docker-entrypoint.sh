f32119468bd846aa4d39dc114207664be2752747
Artur Saganowski <a.saganowski00@gmail.com>
2026-06-28 00:05:49 +0200

    fix railway port

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-cat > /usr/share/nginx/html/assets/env.js << EOF
+# Inject API URL
+cat > /usr/share/nginx/html/assets/env.js << ENVEOF
 (function(window) {
   window.__env = window.__env || {};
   window.__env.apiUrl = '${API_URL:-http://localhost:8080}';
 }(this));
-EOF
+ENVEOF
+
+# Substitute $PORT in nginx config
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
+cp /tmp/default.conf /etc/nginx/conf.d/default.conf
+
 exec nginx -g "daemon off;"
